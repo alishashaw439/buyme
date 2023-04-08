2ad09fd981a1572e6aa385575dc74feccdb74a64
Alisha Shaw <alishashaw439@gmail.com>
2023-04-08 20:50:28 +0530

    first commit

---
+++ frontend/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
