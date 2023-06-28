af9c6286e74eecef12170ab9403f00d6b1cc128c
Alisha Shaw <alishashaw439@gmail.com>
2023-06-28 19:02:21 +0530

    implemented create product api

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,6 +1,8 @@
 import { asyncError } from "../middlewares/error.js";
 import {Product} from "../models/product.js"
 import { ErrorHandler } from "../utils/errorHandler.js";
+import { getDataUri } from "../utils/utils.js";
+import cloudinary from "cloudinary";
 
 export const getAllProducts = asyncError(async(req,res,next)=>{
     const products = await Product.find({})
@@ -16,5 +18,30 @@ export const getProductDetails = asyncError(async(req,res,next)=>{
     res.status(200).json({
         success:true,
         product
+    });
+})
+
+export const createProduct = asyncError(async(req,res,next)=>{
+    const {name,description,category,price,stock} = req.body
+    if(!req.file) return next(new ErrorHandler("Please add image",400))
+    const file = getDataUri(req.file)
+    const myCloud =  await cloudinary.v2.uploader.upload(file.content)
+    const image = {
+        public_id : myCloud.public_id,
+        url: myCloud.secure_url
+    }
+
+    await Product.create({
+        name,
+        description,
+        category,
+        price,
+        stock,
+        images:[image]
+    })
+  
+    res.status(200).json({
+        success:true,
+        message:"product created successfully"
     });
 })
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { isAuthenticated } from "../middlewares/auth.js";
 import { singleUpload } from "../middlewares/multer.js";
-import { getAllProducts, getProductDetails } from "../controllers/product.js";
+import { createProduct, getAllProducts, getProductDetails } from "../controllers/product.js";
 
 const router = express.Router();
 
 router.get("/all",getAllProducts)
 router.route("/single/:id").get(getProductDetails)
+router.post("/new",isAuthenticated,singleUpload,createProduct)
 export default router; 
