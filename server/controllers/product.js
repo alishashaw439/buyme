985ae99e35bd73110131a365e467c2c45e53e2f6
Alisha Shaw <alishashaw439@gmail.com>
2023-06-28 20:12:40 +0530

    added update product, delete product and delete product image

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -45,3 +45,85 @@ export const createProduct = asyncError(async(req,res,next)=>{
         message:"product created successfully"
     });
 })
+
+export const updateProduct = asyncError(async(req,res,next)=>{
+    const {name,description,category,price,stock} = req.body
+
+    const product = await Product.findById(req.params.id)
+    if(!product) return next(new ErrorHandler("Product not found",404))
+
+    if(name) product.name = name
+    if(description) product.description = description
+    if(category) product.category = category
+    if(price) product.price = price
+    if(stock) product.stock = stock
+
+    await product.save()
+    res.status(200).json({
+        success:true,
+        message:"product updated successfully"
+    });
+})
+
+export const deleteProduct = asyncError(async(req,res,next)=>{
+
+    const product = await Product.findById(req.params.id)
+    if(!product) return next(new ErrorHandler("Product not found",404))
+    for(let i=0;i<product.images.length;i++){
+        await cloudinary.v2.uploader.destroy(product.images[i].public_id)
+    }
+
+    await product.deleteOne()
+    res.status(200).json({
+        success:true,
+        message:"product deleted successfully"
+    });
+})
+
+export const addProductImage = asyncError(async(req,res,next)=>{
+    const product = await Product.findById(req.params.id)
+    if(!product) return next(new ErrorHandler("Product not found",404))
+
+    if(!req.file) return next(new ErrorHandler("Please add image",400))
+    const file = getDataUri(req.file)
+    const myCloud =  await cloudinary.v2.uploader.upload(file.content)
+    const image = {
+        public_id : myCloud.public_id,
+        url: myCloud.secure_url
+    }
+
+    product.images.push(image)
+    await product.save()
+  
+    res.status(200).json({
+        success:true,
+        message:"image added successfully"
+    });
+})
+
+export const deleteProductImage = asyncError(async(req,res,next)=>{
+    const product = await Product.findById(req.params.id)
+    if(!product) return next(new ErrorHandler("Product not found",404))
+    
+    const id = req.query.id
+    if(!id) return next(new ErrorHandler("Please enter valid id",400))
+
+    let isExist = -1
+    product.images.forEach((item,index)=>{
+        if(item._id.toString() == id.toString()){
+            isExist = index
+        }
+    })
+    if(isExist < 0){
+        return next(new ErrorHandler("Image doesn't exist",400))
+    }
+
+    await cloudinary.v2.uploader.destroy(product.images[isExist].public_id)
+
+    product.images.splice(isExist,1)
+    await product.save()
+    res.status(200).json({
+        success:true,
+        message:"deleted image successfully"
+    });
+})
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { isAuthenticated } from "../middlewares/auth.js";
 import { singleUpload } from "../middlewares/multer.js";
-import { createProduct, getAllProducts, getProductDetails } from "../controllers/product.js";
+import { addProductImage, createProduct, deleteProduct, deleteProductImage, getAllProducts, getProductDetails, updateProduct } from "../controllers/product.js";
 
 const router = express.Router();
 
 router.get("/all",getAllProducts)
-router.route("/single/:id").get(getProductDetails)
+router.route("/single/:id").get(getProductDetails).put(isAuthenticated,updateProduct).delete(isAuthenticated,deleteProduct)
 router.post("/new",isAuthenticated,singleUpload,createProduct)
+
+router.route("/images/:id").post(isAuthenticated,singleUpload,addProductImage).delete(isAuthenticated,deleteProductImage)
 export default router; 
