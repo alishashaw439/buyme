bac1f768ad7063c5c0f90869e4e501226b8f7a5d
Alisha Shaw <alishashaw439@gmail.com>
2023-07-08 23:26:43 +0530

    added get orders,process orders and order details api

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,6 +1,7 @@
 import {asyncError} from "../middlewares/error.js";
 import {Order} from '../models/order.js';
 import {Product} from '../models/product.js';
+import { ErrorHandler } from "../utils/errorHandler.js";
 
 export const createOrder = asyncError(async(req,res,next)=>{
         const {shippingInfo,orderItems,paymentMethod,paymentInfo,itemsPrice,taxPrice,shippingCharges,totalAmount} = req.body
@@ -25,3 +26,47 @@ export const createOrder = asyncError(async(req,res,next)=>{
             message:"Order Placed Successfully"
         })
 }) 
+
+export const getMyOrders = asyncError(async(req,res,next)=>{
+    const orders = await Order.find({user:req.user._id})
+    res.status(200).json({
+        success:true,
+        orders
+    })
+})
+
+export const getOrderDetails = asyncError(async(req,res,next)=>{
+    const order = await Order.findById(req.params.id)
+    if(!order) return next(new ErrorHandler("Order not found",404))
+    res.status(200).json({
+        success:true,
+        order
+    })
+})
+
+export const getAdminOrders = asyncError(async(req,res,next)=>{
+    const orders = await Order.find({})
+    res.status(200).json({
+        success:true,
+        orders
+    })
+})
+
+export const processOrder = asyncError(async(req,res,next)=>{
+    const order = await Order.findById(req.params.id)
+    if(!order) return next(new ErrorHandler("Order not found",404))
+    if(order.orderStatus === "Preparing"){
+        order.orderStatus = "Shipped"
+    }else if(order.orderStatus === "Shipped"){
+        order.orderStatus = "Delivered"
+        order.deliveredAt = new Date(Date.now())
+    }else{
+        return next(new ErrorHandler("Order already delivered",400))
+    }
+
+    await order.save()
+    res.status(200).json({
+        success:true,
+        message: "Order processed Successfully"
+    })
+})
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import {createOrder } from '../controllers/order.js'
-import { isAuthenticated } from '../middlewares/auth.js'
+import {createOrder, getAdminOrders, getMyOrders, getOrderDetails, processOrder } from '../controllers/order.js'
+import { isAdmin, isAuthenticated } from '../middlewares/auth.js'
 
  const router = express.Router()
 
  router.post("/new",isAuthenticated,createOrder)
+ router.get("/my",isAuthenticated,getMyOrders)
+ router.get("/admin",isAuthenticated,isAdmin,getAdminOrders)
+ router.route("/single/:id").get(isAuthenticated,getOrderDetails).put(isAuthenticated,isAdmin,processOrder)
 
  export default router
